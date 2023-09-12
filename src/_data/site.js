fd37c19e55ace4e7ac5739cbdb7d6bfbe32c57dd
Raul Samperio <raulsdev@gmail.com>
2023-09-12 17:00:28 -0600

    first commit

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
-  keywords: ['eleventy', 'template', 'simple', 'clean'],
+  title: 'Cosas que pasan',
+  description: 'Blog del Rolinski',
+  keywords: ['eleventy', 'random', 'blog'],
   url: 'https://vredeburg.netlify.app', // your site url without trailing slash
-  paginate: 6 // how many posts you want to show for each page
+  paginate: 6, // how many posts you want to show for each page
   // uncomment the next line if you want to add disqus to your site
   // disqusShortname: "your-shortname"
 };
